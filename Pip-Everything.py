9316a5d298de33ec7398371272f40b9e39a2a48c
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2021-01-11 20:56:24 -0700

    Update Pip-Everything.py

--- Pip-Everything.py
+++ Pip-Everything.py
@@ -7,18 +7,20 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", package])
 
 
-def update():
+def update() -> 'Updates pip packages':
     print("Getting packages")
     a = subprocess.Popen([sys.executable, "-m", "pip", "list", "--local", "--outdated", "--format", "json"],
                          stdout=subprocess.PIPE)
     out, err = a.communicate()
     dict = json.loads(out)
     ##parsing plz wait
-    if dict == []:  #check if no updates are available
+    if dict == []:  # check if no updates are available
         print('Nothing to update')
         exit()
-    for x in dict: #installing everything
-        print('Installing {}'.format(x['name']))
-        install(x['name'])
+    else:
+        for x in dict:  # installing everything
+            print('Installing {}'.format(x['name']))
+            install(x['name'])
+
 
 update()
