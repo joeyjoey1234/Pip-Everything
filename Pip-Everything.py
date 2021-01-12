0d3e64ee121f5712e5c92b7273c0c29176b6b7c2
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2021-01-12 16:35:06 -0700

    Added User Choice
    add the feature to allow the user to choose what they dont want

--- Pip-Everything.py
+++ Pip-Everything.py
@@ -8,17 +8,32 @@ def install(package):
 
 
 def update() -> 'Updates pip packages':
+    # list of stuff you dont want.
+    no = []
+    yes_no = 'yes'
     print("Getting packages")
     a = subprocess.Popen([sys.executable, "-m", "pip", "list", "--local", "--outdated", "--format", "json"],
                          stdout=subprocess.PIPE)
     out, err = a.communicate()
     dict = json.loads(out)
-    ##parsing plz wait
-    if dict == []:  # check if no updates are available
+    # parsing plz wait
+
+    if dict == [1]:  # check if no updates are available
         print('Nothing to update')
         exit()
-    else:
-        for x in dict:  # installing everything
+        # lets you pick what you don't want(in case version issue in PROD)
+    elif yes_no != 'no':
+        for x in dict:
+            print(x['name'])
+        while yes_no != 'no':
+            dont_want: 'user input' = input("Anything you dont want? (Package name or no) ")
+            no.append(dont_want)
+            yes_no = dont_want
+
+    for x in dict:  # installing everything
+        if x["name"] in no:
+            pass
+        else:
             print('Installing {}'.format(x['name']))
             install(x['name'])
 
